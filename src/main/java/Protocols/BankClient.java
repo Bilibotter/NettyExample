bc0c16709e6f317c131cb0968794aaff970d115d
Bilibotter <1102043797@qq.com>
2021-02-22 17:35:06 +0800

    GoogleProtocol序列化

--- src/main/java/Protocols/BankClient.java
+++ src/main/java/Protocols/BankClient.java
@@ -13,6 +13,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class BankClient {
+
     final static ChannelInitializer<SocketChannel> initializer;
     static  {
         initializer = new BankClientInitializer();
